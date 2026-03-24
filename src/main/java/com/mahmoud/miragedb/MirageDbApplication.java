5b0f2660d6e32a762d4c3b51c024134ce0dd2052
Mahmoud Abdulmawlaa <m.elbaadishy@gmail.com>
2026-03-24 20:57:30 +0200

    project setup

---
+++ src/main/java/com/mahmoud/miragedb/MirageDbApplication.java
@@ -0,0 +1,13 @@
+package com.mahmoud.miragedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirageDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MirageDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahmoud/miragedb/MirageDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmoud.miragedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirageDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
